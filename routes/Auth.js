4ce5528d6492ce059e02ba16537cbbc81d002a24
hominhhau <hauxoan333@gmail.com>
2024-10-31 14:40:11 +0700

    demo connect

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
+
 router.post('/loginByPhone', authController.loginByPhone);
 router.post('/registerByPhone', authController.registerByPhone);
 
